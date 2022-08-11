6af26594cb8057f12afeb12587d5d5693ac45e56
Woodrow Wilson <65731874+potus28@users.noreply.github.com>
2022-08-11 11:46:01 -0500

    Update libxc.sh

--- Linux/libxc.sh
+++ Linux/libxc.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+libxc_version=5.2.3
+wget http://www.tddft.org/programs/libxc/down.php?file=$libxc_version/libxc-$libxc_version.tar.gz
+
+tar -xzvf down.php?file=$libxc_version%2Flibxc-$libxc_version.tar.gz
+mv libxc-$libxc_version $libxc_version
+
+cd $libxc_version
 mkdir -p install
 mkdir -p build
 
@@ -7,4 +14,4 @@ prefix=$(pwd)/install
 cd build
 cmake .. -DCMAKE_INSTALL_PREFIX=$prefix -DENABLE_FORTRAN=ON
 
-make -j 12 && make install
+`make && make install
