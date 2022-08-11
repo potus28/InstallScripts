b7f5e2b04852f32fe923ef93976bb83bf0cedbf5
Woodrow Wilson <65731874+potus28@users.noreply.github.com>
2022-08-11 11:41:47 -0500

    Update libint.sh

--- Linux/libint.sh
+++ Linux/libint.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+libint_version=2.6.0-cp2k-lmax-7
+wget https://github.com/cp2k/libint-cp2k/releases/download/v2.6.0/libint-v$libint_version.tgz
+tar -xzvf libint-v2.6.0-cp2k-lmax-7.tgz
+
+mv libint-v2.6.0-cp2k-lmax-7 $libint_version
+cd $libint_version
+
 mkdir build
 mkdir install
 prefix=$(pwd)/install
@@ -8,5 +15,4 @@ cd build
 
 cmake .. -DCMAKE_INSTALL_PREFIX=$prefix -DENABLE_FORTRAN=ON
 
-make -j 12
-make install
+make && make install
